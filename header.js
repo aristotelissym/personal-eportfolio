9b13e2f1c5b6c741cb0a71845f160a7dbd699e0a
aristotelissym <arisymeon97@gmail.com>
2023-05-19 17:55:37 +0300

    finalize first version

--- header.js
+++ header.js
@@ -3,31 +3,37 @@ let appHeader = `
 <nav class="navbar is-transparent"
 style="margin-left: 50px; margin-right: 50px; padding-top: 4px; padding-bottom: 4px;">
 <div class="navbar-brand">
-  <a class="navbar-item" href="./index.html" style="font-size: larger; font-weight: 500;">
+  <a class="navbar-item" href="./index.html" style="font-size: larger; font-weight: 600;">
     <i class="fa-solid fa-terminal" style="margin-right: 8px;"></i>
     Aristotelis.
   </a>
+  <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu-header">
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+  </a>
 </div>
 
-<div id="navbar-menu" class="navbar-menu">
+<div class="navbar-menu" id="navbar-menu-header">
   <div class="navbar-end">
-   <a class="navbar-item" href="./whoami.html">
-    $ whoami
-  </a>
-  <a class="navbar-item" href="./projects.html">
-    Projects
-  </a>
-  <a class="navbar-item" href="./blog.html">
-    Blog
-  </a>
+  <!-- <a class="navbar-item" href="./whoami.html">
+     $ whoami
+    </a>
+    <a class="navbar-item" href="./projects.html">
+      Projects
+    </a>
+    <a class="navbar-item" href="./blog.html">
+      Blog
+    </a>
     <a class="navbar-item" href="./contact.html">
       <button class="button is-rounded" style="background-color: hsl(246, 75%, 52%); color: antiquewhite;">
         Let's Chat
       </button>
-    </a>
+    </a> -->
   </div>
 </div>
 </nav>
+
 `;
 
 // Assign Template to Element.
